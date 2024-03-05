05007089dfc63e83b78d03286d2441fd3cccad26
BPBoyle <50708089+bpboyle@users.noreply.github.com>
2024-03-05 13:14:30 +0000

    sampling app

--- pol2017_content/app.R
+++ pol2017_content/app.R
@@ -1,51 +1,133 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(ggplot2)
+library(patchwork)
+library(dplyr)
 
-# Define UI for application that draws a histogram
+
+
+
+
+
+# Define UI for app that draws a histogram ----
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # App title ----
+  titlePanel("Simple random sampling"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      # sliderInput(inputId = "sample_n",
+      #             label = "Sample size:",
+      #             min = 0,
+      #             max = 1000,
+      #             value = 0, step = 1, ticks = T),
+      
+      numericInput(inputId = 'sample_n', label = 'Sample size', value = 0, min = 0, max = 10000, step = NA)  
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      p("Population = 100,000"),
+      textOutput("sample_n_text"),
+      
+      # Output: Histogram ----
+      plotOutput(outputId = "samplePlot")
+      
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a histogram ----
 server <- function(input, output) {
+  
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  
+  output$sample_n_text <- renderText({ 
+    paste("Sample size =", input$sample_n)
+  })
+  
+  
+  
+  output$samplePlot <- renderPlot({
+  
+  set.seed(2017)  
+  groups <- letters[1:5] |> toupper()
+  pop <- sample(groups, 100000, replace = T, prob = c(.2, .3, .05, .1, .4))
+  pop_df <- table(pop) |> as.data.frame()
+  
+  pop_plot <- pop_df |> 
+  ggplot(aes(x = pop, y = Freq, fill = factor(pop))) +
+  geom_col() +
+    theme_minimal() +
+    scale_fill_manual(values = c('#E69F00','#56B4E9','#CC79A7','#009E73','#D55E00')) +
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major.x = element_blank(),
+          text = element_text(size = 16)) +
+    guides(fill = 'none') +
+    labs(x = 'Group', y = 'Count', title = 'Population')
+  ###
+  sample_n <- input$sample_n
+  if(is.na(sample_n)){
+    sample_n <- 0
+  }
+  
+  
+  sample <- sample(pop, sample_n)
+  sample_df <- table(sample) |> as.data.frame()
+  
+  
+  sample_fill <- data.frame(sample = groups, Freq = 0)
+  sample_expand <- sample_fill[which(!groups %in% sample_df$sample),]
+  
+  if(dim(sample_expand)[1] > 0){
+     sample_df <- rbind(sample_df, sample_expand)  
+  }
+  
+  sample_df$sample <- factor(sample_df$sample, levels = letters[1:5] |> toupper())
+  
+  
+  
+  
+  sample_plot <- sample_df |> 
+    ggplot(aes(x = sample, y = Freq, fill = factor(sample))) +
+    geom_col() +
+    theme_minimal() +
+    scale_fill_manual(values = c('#E69F00','#56B4E9','#CC79A7','#009E73','#D55E00')) +
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major.x = element_blank(),
+          text = element_text(size = 16)) +
+    guides(fill = 'none') +
+    labs(x = 'Group', y = 'Count', title = 'Sample')
+  
+  
+  
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  if(sample_n == 0){
+    sample_plot <- sample_plot + theme_void()
+  } 
+  
+  pop_plot + sample_plot
+  
+  
+  
+    
+  })
+  
 }
 
-# Run the application 
+# Create Shiny app ----
 shinyApp(ui = ui, server = server)
